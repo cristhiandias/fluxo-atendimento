0607e603b860b890fccfaf843b2075935c06b69b
Cristhian Dias <c.ribeirodias@hotmail.com>
2018-07-04 16:55:38 -0300

    Neymar em pé/caído

--- src/app/cliente/cadastro-cliente/cadastro-cliente.component.ts
+++ src/app/cliente/cadastro-cliente/cadastro-cliente.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroClienteComponent implements OnInit {
 
+  cliente: string ="Proprietário";
+  contador : number = 0;
+  imagem: string;
   constructor() { }
 
   ngOnInit() {
+    let self = this;
+    setTimeout(() =>{
+      self.cliente = "Proprietário iniciando...";
+      self.imagem = "assets/neymarempe.jpg";
+    }, 1000);
+    let interval = setInterval(() =>{
+      self.cliente = "Proprietário " + self.contador++;
+      if (self.contador ==5){
+        clearInterval(interval);
+        self.imagem = "assets/neymarcaido.jpg";
+      }    
+    }, 2000);
+  }
+
+  levantarNeymar() : void {
+    this.imagem = "assets/neymarempe.jpg";
   }
 
 }
